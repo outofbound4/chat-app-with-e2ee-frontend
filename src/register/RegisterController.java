41b459db70ab067124d94444b5b315bb7b28c5dc
GAURAV KUMAR <gaurav.kr109@gmail.com>
2020-12-20 23:04:41 +0530

    File updated

---
+++ src/register/RegisterController.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package register;
+
+import com.jfoenix.controls.JFXTextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author gaurav
+ */
+public class RegisterController implements Initializable {
+
+    @FXML
+    private JFXTextField firstName;
+    @FXML
+    private JFXTextField lastName;
+    @FXML
+    private JFXTextField email;
+    @FXML
+    private JFXTextField password;
+    @FXML
+    private JFXTextField confirmPassword;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void onTermsAndConditionClck(ActionEvent event) {
+    }
+
+    @FXML
+    private void onRegisterButtonClick(ActionEvent event) {
+    }
+    
+}
