7258ddbd0a2f83428aada447d888d9db3505ea0a
GAURAV KUMAR <gaurav.kr109@gmail.com>
2020-12-21 09:32:58 +0530

    File updated

--- src/register/RegisterController.java
+++ src/register/RegisterController.java
@@ -6,11 +6,17 @@
 package register;
 
 import com.jfoenix.controls.JFXTextField;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -36,7 +42,7 @@ public class RegisterController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 
     @FXML
     private void onTermsAndConditionClck(ActionEvent event) {
@@ -45,5 +51,20 @@ public class RegisterController implements Initializable {
     @FXML
     private void onRegisterButtonClick(ActionEvent event) {
     }
-    
+
+    @FXML
+    private void onLoginClick(ActionEvent event) {
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("../login/login.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Register");
+            stage.setScene(new Scene(root));
+            stage.show();
+            //it Hides the current window
+            ((Node) (event.getSource())).getScene().getWindow().hide();
+        } catch (IOException e) {
+        }
+    }
+
 }
