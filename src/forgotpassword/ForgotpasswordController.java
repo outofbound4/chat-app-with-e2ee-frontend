75174eb273dd58270ce3695bf9cc69d3725650ad
GAURAV KUMAR <gaurav.kr109@gmail.com>
2021-01-08 17:11:13 +0530

    File updated

--- src/forgotpassword/ForgotpasswordController.java
+++ src/forgotpassword/ForgotpasswordController.java
@@ -10,6 +10,7 @@ import com.jfoenix.controls.JFXTextField;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.regex.Pattern;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -17,6 +18,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -28,6 +31,8 @@ public class ForgotpasswordController implements Initializable {
 
     @FXML
     private JFXTextField email;
+    @FXML
+    private Text error;
 
     /**
      * Initializes the controller class.
@@ -40,7 +45,36 @@ public class ForgotpasswordController implements Initializable {
     @FXML
     private void onGetNewPasswordClick(ActionEvent event) {
         JavaMail mail = new JavaMail();
-        System.out.println(mail.sendMail("gaurav.kr109@gmail.com", "testing", "testing"));
+        String resetEmail = email.getText();
+        if (isValidEmail(resetEmail)) {
+            String status = mail.sendMail(resetEmail, "testing", "testing");
+            if (status.equals("Successful")) {
+                error.setText("Email sent successfully..!!");
+            } else {
+                error.setText(status);
+            }
+        } else {
+            error.setText("Invalid Email.");
+        }
+    }
+
+    /**
+     *
+     * @param email
+     * @return
+     */
+    //    method for email validation
+    private boolean isValidEmail(String email) {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."
+                + "[a-zA-Z0-9_+&*-]+)*@"
+                + "(?:[a-zA-Z0-9-]+\\.)+[a-z"
+                + "A-Z]{2,7}$";
+
+        Pattern pat = Pattern.compile(emailRegex);
+        if (email == null) {
+            return false;
+        }
+        return pat.matcher(email).matches();
     }
 
     @FXML
@@ -56,6 +90,11 @@ public class ForgotpasswordController implements Initializable {
             ((Node) (event.getSource())).getScene().getWindow().hide();
         } catch (IOException e) {
         }
+    }
+
+    @FXML
+    private void clearError(MouseEvent event) {
+        error.setText("");
     }
 
 }
