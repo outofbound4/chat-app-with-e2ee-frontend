0f1db4f5d3e834c64168a953f963c6ccde0b8a6e
GAURAV KUMAR <gaurav.kr109@gmail.com>
2021-01-08 21:17:04 +0530

    File updated

---
+++ src/HTTPCall/ForgotPassword.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HTTPCall;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ *
+ * @author gaurav
+ */
+public class ForgotPassword {
+
+    @SerializedName("email")
+    public String email;
+    @SerializedName("response")
+    public String response;
+    @SerializedName("message")
+    public String message;
+
+    public ForgotPassword(String email) {
+        this.email = email;
+    }
+}
